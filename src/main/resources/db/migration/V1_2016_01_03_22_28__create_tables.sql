fd857602826db4151580f9c2d3b43b1426587d4f
SuXiaoKai <sukai.snake@gmail.com>
2016-01-07 09:38:35 +0800

    use flyway - SK

--- src/main/resources/db/migration/V1_2016_01_03_22_28__create_tables.sql
+++ src/main/resources/db/migration/V1_2016_01_03_22_28__create_tables.sql
@@ -1,3 +1,10 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE user (
+  id        VARCHAR(36) NOT NULL,
+  create_at DATETIME,
+  CONSTRAINT user PRIMARY KEY (id)
+);
+
 DROP TABLE IF EXISTS `user_board_status`;
 CREATE TABLE user_board_status (
   id      VARCHAR(36)  NOT NULL,
@@ -6,11 +13,3 @@ CREATE TABLE user_board_status (
   CONSTRAINT user_board_status_pk PRIMARY KEY (id),
   CONSTRAINT user_board_status_fk_1 FOREIGN KEY (user_id) REFERENCES user (id)
 );
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE user (
-  id        VARCHAR(36) NOT NULL,
-  create_at DATETIME,
-  CONSTRAINT user PRIMARY KEY (id)
-);
-
