4de00d81f8eb29ec3855993fcfdf6d17e1b00aa2
jie.liu <jieliu@thoughtworks.com>
2016-01-08 10:09:12 +0800

    add function test framework

---
+++ src/test/java/org/k2/controller/K2ControllerTest.java
@@ -0,0 +1,34 @@
+package org.k2.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/applicationContext.xml"})
+public class K2ControllerTest {
+
+    @Test
+    public void testRegister() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testGetScore() throws Exception {
+
+    }
+
+    @Test
+    public void testGetScores() throws Exception {
+
+    }
+}
