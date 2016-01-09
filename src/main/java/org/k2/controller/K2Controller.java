e2549aca232e0b14c73c2ae9edd71e09f07b4ec9
jie.liu <jieliu@thoughtworks.com>
2016-01-09 10:11:04 +0800

    add produce json type

--- src/main/java/org/k2/controller/K2Controller.java
+++ src/main/java/org/k2/controller/K2Controller.java
@@ -6,6 +6,7 @@ import org.k2.validation.DirectionValidation;
 import org.k2.validation.UserNameValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +16,9 @@ public class K2Controller {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value = "/register/{who}", method = RequestMethod.POST)
+    @RequestMapping(value = "/register/{who}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<String> register(@PathVariable("who") @UserNameValidation String who) {
-
         try {
             userService.persistUser(who);
         } catch (Exception e) {
@@ -27,21 +27,21 @@ public class K2Controller {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @RequestMapping(value = "/move/{direction}", method = RequestMethod.POST)
+    @RequestMapping(value = "/move/{direction}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<String> move(@PathVariable("direction") @DirectionValidation String direction) {
 
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
-    @RequestMapping(value = "/score/{user}", method = RequestMethod.GET)
+    @RequestMapping(value = "/score/{user}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<String> getScore(@PathVariable("user") @UserNameValidation String user) {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/scores", method = RequestMethod.GET)
+    @RequestMapping(value = "/scores", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<String> getScores(@RequestParam("offset") int offset, @RequestParam("size") int size) {
 
